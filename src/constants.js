33a9a2878bf760de48780af9b8796046d55fb22b
Iris94 <mirzairis@proton.me>
2026-06-14 16:14:04 +0200

    Refactor code structure for improved readability and maintainability

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 export const SITE = {
   QIKO: "https://qiko-chat.space",
+  TETRIS: "https://iris94.github.io/tetris-gameboy-edition/",
+  EMPIRE: "https://empire-cwwxxi9p7-iris94.vercel.app/",
   LINKEDIN: "https://www.linkedin.com/in/iris94/",
   GITHUB: "https://github.com/Iris94",
 };
